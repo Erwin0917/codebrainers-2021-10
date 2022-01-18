513a315849941a5683640594088c28e509aa9b8e
Erwin Heba <erwin.heba@twoupdigital.com>
2022-01-18 17:41:08 +0100

    exercise-01 - build base structure

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+//string
+"abc";
+'abc';
+`abc ${}`;
+
+//number
+2
+2.33
+
+//boolean
+true/false
+
+undefined / null
+
+//array
+[]
+
+//object
+{}
+
+
+function myFnName(name, b = 'surname') {
+
+}
+
+const mySecName = (name, b) => {
+
+}
+
+myFnName(200, 'ad');
+mySecName();
+
+
+var oldType;
+
+const constType = {};
+constType.name = 'name';
+const arrayExample = [];
+arrayExample[0] = 'name';
+
+let letType;
+
+== //Operator porównania
+=== // Operator identyczności
+
