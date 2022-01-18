5cbe009c7c575a216f2f33a44c530ccda6cb97c4
kostek437 <kostek@sobanek.pl>
2022-01-18 19:28:03 +0100

    exercise-02 - prompt, alert, if else

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,21 @@
 //Ile subów przybywa w ciągu miesiąca
 //Obliczyć ile przybędzie w ciągu roku
 //Ile wyświetleń w miesiąc i ile zarobi per rok - 1 wyświetlenie = 1gr
+
+const subscribers = prompt("How many subscribers do you have?");
+console.log(subscribers);
+
+if (subscribers > 100 && subscribers < 1000) {
+    alert("Silver badge");
+
+} else if (subscribers >= 1000 && subscribers < 10000) {
+    alert("Gold badge");
+
+
+} else if (subscribers >= 10000) {
+    alert("Diamond badge");
+
+} else{
+    const x = 100 - subscribers;
+    alert(`You are: ${x} off from the Silver badge `);
+}
