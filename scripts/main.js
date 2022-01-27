4043c0eeca26b71d6e32f2d7d2b56dc75a1e2858
Erwin Heba <erwin.heba@twoupdigital.com>
2022-01-27 20:23:35 +0100

    Exercise-05 - POJO, Class, get, set

--- scripts/main.js
+++ scripts/main.js
@@ -1,171 +1,46 @@
-function getRandomNumberBetween(min, max) {
-    return Math.floor(Math.random() * (max - min * 1) + min);
-}
-function array () {
-    const arr = [];
-    const nonEmptyArr = ['kasia', 'janek', 'zosia', [1, 3, 4]];
-    const arrExample = new Array(5);
+// POJO = Plain Old Javascript Object
 
-// console.log(nonEmptyArr[0]);
-
-// console.log(arrExample);
-
-//push() pop()
-    console.log(nonEmptyArr.push('dawid'));
-    console.log(nonEmptyArr);
-
-    console.log(nonEmptyArr.pop());
-    console.log(nonEmptyArr);
-
-//unshift() shift()
-    console.log(nonEmptyArr.unshift('ernest'));
-    console.log(nonEmptyArr);
-
-    nonEmptyArr[3][2];
-
-    console.log("========================");
-    console.log(nonEmptyArr.splice(2, 2));
-    const clone = nonEmptyArr;
-    console.log(nonEmptyArr);
-
-    console.log("========================");
-    console.log(nonEmptyArr.indexOf('ernest'));
-    console.log(nonEmptyArr.indexOf('adam'));
-
-    console.log(nonEmptyArr.includes('ernest'));
-    console.log(nonEmptyArr.includes('adam'));
-
+const car = {
+    color: 'red',
+    maximumSpeed: 250,
+    numberOfWheels: 4
 }
 
-//Maszyna losująca ma 49 nr ,
-// losujemy 6 piłeczek
-// Podajemy kupon z wpisanymi naszymi 6 numerami
-//Ile razy ma miejsce losowanie aby nasze 6 cyfr było wylosowane?
+class Car {
 
-function lottery() {
-    const comparator = (a, b) => {
-    return a - b;
-}
-
-const compareArrays = (a, b) => {
-    if(a.length !== b.length){
-        console.error('Wrong arrays length');
-        return 'wrong arrays';
-    }
-    for(let i = 0; i < a.length; i++){
-        if(a[i] !== b[i]){
-            return false;
+    constructor(color = 'black', maximumSpeed, numberOfWheels) {
+        if (typeof color === 'string' && typeof maximumSpeed === 'number' && typeof numberOfWheels === 'number') {
+            this.color = color;
+            this.maximumSpeed = maximumSpeed;
+            this.numberOfWheels = numberOfWheels;
         }
+
+        this.speed = 0;
     }
 
-    return true;
-}
+    setSpeed(speed) {
 
-
-
-function fillNumbers() {
-    const numbers = [];
-    for (let i = 1; i <= 49; i++) {
-        numbers.push(i);
-    }
-    return numbers;
-}
-
-function drownNumbers(numbers) {
-    const myDrownNumbers = [];
-    for (let i = 1; i <= 6; i++) {
-        const indexNumber = getRandomNumberBetween(0, 49 - i);
-        const spliceCatcher = numbers.splice(indexNumber, 1);
-        myDrownNumbers.push(...spliceCatcher);
-    }
-    return myDrownNumbers;
-}
-
-
-
-// Lottery ----------
-
-
-let myNumbers = [27, 13, 11, 1, 9, 40];
-let counter = 0;
-let jackpot = false;
-
-do {
-    counter++;
-
-    const numbers = fillNumbers();
-    const myDrownNumbers = drownNumbers(numbers);
-
-    const sortedMyDrownNumbers = myDrownNumbers.sort(comparator);
-    const sortedMyNumbers = myNumbers.sort(comparator);
-    jackpot = compareArrays(sortedMyDrownNumbers, sortedMyNumbers);
-
-    if (jackpot === 'wrong arrays') {
-        break;
     }
 
-    // console.log('-------------');
-    // console.log('init numbers', numbers);
-    // console.log('myDrownNumbers', myDrownNumbers);
-    // console.log('myDrownNumbers length', myDrownNumbers.length);
-    // console.log('myNumbers', myNumbers);
-    // console.log('='.repeat(20));
-} while (!jackpot)
+    accelerate(howMuch) {
 
-console.log('counter', counter);
-}
-// lottery()
-
-
-
-// const mapCallback = (value, index) => {
-//     // console.log('value', value);
-//     // console.log('index', index);
-//     return value;
-// }
-//
-// myNumbers.forEach(function (value, index) {
-//     // console.log('value', value);
-//     // console.log('index', index);
-// });
-//
-// const newArray = myNumbers.map((value, index) => {
-//     // console.log('value', value);
-//     // console.log('index', index);
-//     return value;
-// });
-//
-// myNumbers.map(mapCallback);
-//
-// //
-// const filteredArray = myNumbers.filter(value => value === 13);
-//
-// // console.log('filteredArray', filteredArray);
-// // myNumbers.sort()
-// const findArray = myNumbers.find(value => value === 13);
-// console.log('findArray', findArray);
-
-
-
-const arrayOfString = ['krystian', 'jaś', 'przemysław', 'ala', 'barnaba', 'kasia', 3, 4, 7, '9', NaN, null, undefined];
-
-const nameLengthMoreThanFive = name => (typeof name === 'string' && name.length > 5);
-
-
-const names = arrayOfString.filter(nameLengthMoreThanFive);
-
-function filterNumbers(value){
-    if(typeof value === 'number'){
-        return value;
     }
+
+    decelerate(howMuch) {
+
+    }
+
 }
 
-function square(number){
-    return number * number;
-}
 
-const arrayOfNumbers = arrayOfString.filter(filterNumbers).map(square);
+const car1 = new Car();
+car1.setSpeed(200);
+console.log('car1 setSpeed with number', car1);
+car1.setSpeed('200');
+console.log('car1 setSpeed with string', car1);
 
+const car2 = new Car(2, '2', 2);
+console.log(car2);
 
-console.log(names);
-console.log(arrayOfNumbers);
+const car3 = new Car('2', 2);
+console.log(car3);
