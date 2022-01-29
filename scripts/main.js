6836a761680b96bf84db60514537e923bdc158a4
kostek437 <kostek@sobanek.pl>
2022-01-29 09:22:17 +0100

    Excercise-05 Homework solution

--- scripts/main.js
+++ scripts/main.js
@@ -35,11 +35,25 @@ class Car {
     }
 
     accelerate(howMuch) {
-    // todo: to finish
+        if (this.isNumber(howMuch)) {
+            if (this.speed + howMuch <= this.maximumSpeed) {
+                this.speed += howMuch ;
+            } else {
+                this.speed = this.maximumSpeed
+                console.log("You can't accelerate that much.");
+            }
+        }
     }
 
     decelerate(howMuch) {
-    // todo: to finish
+        if (this.isNumber(howMuch)) {
+            if (this.speed - howMuch >= 0) {
+                this.speed -= howMuch ;
+            } else {
+                this.speed = 0;
+                console.log("You can't go slower than 0.");
+            }
+        }
     }
 
 }
@@ -47,12 +61,25 @@ class Car {
 
 const car1 = new Car();
 car1.setSpeed(200);
-car1.setSpeed(301);
+// car1.setSpeed(301);
 console.log('car1 setSpeed with number', car1);
-car1.setSpeed(NaN);
-console.log('car1 setSpeed with number', car1);
-car1.setSpeed('200');
-console.log('car1 setSpeed with string', car1);
+
+car1.accelerate(500)
+console.log('car1 accelerate', car1);
+
+car1.decelerate(300)
+console.log('car1 decelerate', car1);
+
+
+
+
+
+
+
+// car1.setSpeed(NaN);
+// console.log('car1 setSpeed with number', car1);
+// car1.setSpeed('200');
+// console.log('car1 setSpeed with string', car1);
 
 // const car2 = new Car(2, '2', 2);
 // console.log(car2);
