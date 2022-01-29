bd9f9aa1d7eea32746acce64d7ca224ae97c1b30
Piotr Hołowienko <piotrholowienkodev@gmail.com>
2022-01-29 12:59:04 +0100

    exercise-06 - teams,

--- scripts/main.js
+++ scripts/main.js
@@ -3,23 +3,40 @@ import { Hero, Villain } from './person.js';
 function getRandomNumberBetween(min, max) {
     return Math.floor(Math.random() * (max - min * 1) + min);
 }
+const heroTeam = [];
+const villainTeam = [];
 
+function isTeamAlive(team) {
+    let teamIsAlive = false;
+    for (const element of team) {
+        console.log(element);
+        if (element.isAlive()) {
+            teamIsAlive = true;
+        }
+    }
+    return teamIsAlive;
+}
 
 const hero = new Hero(getRandomNumberBetween(50,100), getRandomNumberBetween(1,5));
 const villain = new Villain(getRandomNumberBetween(50,100), getRandomNumberBetween(1,5));
-console.log(villain)
 
-while(hero.isAlive() && villain.isAlive()){
-    if(villain.isAlive()){
-        villain.attack(hero, getRandomNumberBetween(10,25));
-    }
-    if(hero.isAlive()){
-        hero.attack(villain, getRandomNumberBetween(15,25));
-    }
-    console.log("Hero has " + hero.hitPoints + " hp left")
-    console.log("Villain has " + villain.hitPoints + " hp left")
+heroTeam.push(hero);
+villainTeam.push(villain);
+
+console.log(isTeamAlive(heroTeam));
+console.log(isTeamAlive(villainTeam));
+
+function duel(attacker,defender) {
+
 }
-
-
-
+// while(hero.isAlive() && villain.isAlive()){
+//     if(villain.isAlive()){
+//         villain.attack(hero, getRandomNumberBetween(10,25));
+//     }
+//     if(hero.isAlive()){
+//         hero.attack(villain, getRandomNumberBetween(15,25));
+//     }
+//     console.log("Hero has " + hero.hitPoints + " hp left")
+//     console.log("Villain has " + villain.hitPoints + " hp left")
+// }
 
