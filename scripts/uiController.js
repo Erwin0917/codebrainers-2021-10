05c0f23530cc6208f3a6ec989c5246d6a60d5b06
Klaudia Brzywcy <kbrzywcy@gmail.com>
2022-01-31 14:06:23 +0100

    Exercise 06 - homework stage 1

--- scripts/uiController.js
+++ scripts/uiController.js
@@ -2,16 +2,25 @@ export class UiController {
     constructor() {
         const nameInput = document.querySelector('#name');
         const nameRandomButton = document.querySelector('#random-name');
+        const weaponInput = document.querySelector('#weapon');
+        const weaponRandomButton = document.querySelector('#random-weapon');
 
         //TODO: Wyszukać resztę elementów z UI i dodać o nich odpowiednie listenery
+        //Sekcja z inputem
         nameInput.addEventListener('change', () => {
             console.log('Name input was change');
         })
+        weaponInput.addEventListener('change', () => {
+            console.log('Weapon input was changed');
+        })
+        //Sekcja z kliknięciem
 
         nameRandomButton.addEventListener('click', () => {
             //TODO: na click uzupełnij nameInput losowym imieniem
             console.log('Random Name button click');
         })
+
+
 
     }
 }
