924c945c3e7b37866f56ee674d1f6c414cfaed93
Erwin Heba <erwin.heba@twoupdigital.com>
2022-02-01 18:34:05 +0100

    Exercise-06 - Homework refactor, fix bug with defenderIndex

--- scripts/uiController.js
+++ scripts/uiController.js
@@ -3,87 +3,84 @@ function getRandomNumberBetween(min, max) {
 }
 
 export class UiController {
-    constructor() {
-        const nameInput = document.querySelector('#name');
-        const nameRandomButton = document.querySelector('#random-name');
-        const weaponInput = document.querySelector('#weapon')
-        const weaponRandomButton = document.querySelector('#random-weapon');
-        const strengthInput = document.querySelector('#strength');
-        const strengthRandomButton = document.querySelector('#random-strength');
-        const hpInput = document.querySelector('#hitpoints');
-        const hpRandomButton = document.querySelector('#random-hitpoints');
-        console.log(hpRandomButton)
+    constructor(uiWrapperHtmlClass) {
+        this.uiWrapper = document.querySelector(uiWrapperHtmlClass);
+        this.nameInput = this.uiWrapper.querySelector('#name');
+        this.nameRandomButton = this.uiWrapper.querySelector('#random-name');
+        this.weaponInput = this.uiWrapper.querySelector('#weapon');
+        this.weaponRandomButton = this.uiWrapper.querySelector('#random-weapon');
+        this.strengthInput = this.uiWrapper.querySelector('#strength');
+        this.strengthRandomButton = this.uiWrapper.querySelector('#random-strength');
+        this.hpInput = this.uiWrapper.querySelector('#hitpoints');
+        this.hpRandomButton = this.uiWrapper.querySelector('#random-hitpoints');
 
-
-        nameInput.addEventListener('change', () => {
-            console.log('Name input was change');
-        })
-
-        nameRandomButton.addEventListener('click', () => {
-            let randomName = charName[getRandomNumberBetween(0, 24)]
-            nameInput.value = randomName
-            console.log('Random Name button click');
-            console.log(randomName)
-        })
-        weaponInput.addEventListener('change', () => {
-
-            console.log('Weapon input was change');
-
-        })
-
-        weaponRandomButton.addEventListener('click', () => {
-            let randomWeapon = charWeapon[getRandomNumberBetween(0, 24)]
-            weaponInput.value = randomWeapon
-            console.log('Random weapon button click');
-            console.log(randomWeapon)
-        })
-        strengthInput.addEventListener('change', () => {
-            console.log('strength input was change');
-        })
-
-        strengthRandomButton.addEventListener('click', () => {
-            let randomStrength = getRandomNumberBetween(1, 10)
-            strengthInput.value = randomStrength
-            console.log('Strength Name button click');
-            console.log(randomStrength)
-        })
-        hpInput.addEventListener('change', () => {
-            console.log('Hp input was change');
-        })
-        hpRandomButton.addEventListener('click', () => {
-            let randomHp = getRandomNumberBetween(50, 100)
-            hpInput.value = randomHp
-            console.log('Random hp button click');
-
-
-        })
-
+        this.initAllEventListeners();
     }
+
+
+    initAllEventListeners = () => {
+        this.nameInput.addEventListener('change', () => {
+            console.log('Name input was change');
+        });
+
+        this.nameRandomButton.addEventListener('click', () => {
+            this.nameInput.value = charName[getRandomNumberBetween(0, 24)];
+            console.log('Random Name button click');
+        });
+
+        this.weaponInput.addEventListener('change', () => {
+            console.log('Weapon input was change');
+        });
+
+        this.weaponRandomButton.addEventListener('click', () => {
+            this.weaponInput.value = charWeapon[getRandomNumberBetween(0, 24)];
+            console.log('Random weapon button click');
+        });
+
+        this.strengthInput.addEventListener('change', () => {
+            console.log('strength input was change');
+        });
+
+        this.strengthRandomButton.addEventListener('click', () => {
+            this.strengthInput.value = getRandomNumberBetween(1, 10);
+            console.log('Strength Name button click');
+        });
+
+        this.hpInput.addEventListener('change', () => {
+            console.log('Hp input was change');
+        });
+
+        this.hpRandomButton.addEventListener('click', () => {
+            this.hpInput.value = getRandomNumberBetween(50, 100);
+            console.log('Random hp button click');
+        });
+    };
 }
 
-const charName = ["Harry", "Ross",
-    "Bruce", "Cook",
-    "Carolyn", "Morgan",
-    "Albert", "Walker",
-    "Randy", "Reed",
-    "Larry", "Barnes",
-    "Lois", "Wilson",
-    "Jesse", "Campbell",
-    "Ernest", "Rogers",
-    "Theresa", "Patterson",
-    "Henry", "Simmons",
-    "Michelle", "Perry",
-    "Frank"];
-const charWeapon = ["Brooks",
-    "Rachel", "Edwards",
-    "Christopher", "Perez",
-    "Thomas", "Baker",
-    "Sara", "Moore",
-    "Chris", "Bailey",
-    "Roger", "Johnson",
-    "Marilyn", "Thompson",
-    "Anthony", "Evans",
-    "Julie", "Hall",
-    "Paula", "Phillips",
-    "Annie", "Hernandez",
-    "Dorothy", "Murphy"];
+const charName = ['Harry', 'Ross',
+    'Bruce', 'Cook',
+    'Carolyn', 'Morgan',
+    'Albert', 'Walker',
+    'Randy', 'Reed',
+    'Larry', 'Barnes',
+    'Lois', 'Wilson',
+    'Jesse', 'Campbell',
+    'Ernest', 'Rogers',
+    'Theresa', 'Patterson',
+    'Henry', 'Simmons',
+    'Michelle', 'Perry',
+    'Frank'];
+
+const charWeapon = ['Brooks',
+    'Rachel', 'Edwards',
+    'Christopher', 'Perez',
+    'Thomas', 'Baker',
+    'Sara', 'Moore',
+    'Chris', 'Bailey',
+    'Roger', 'Johnson',
+    'Marilyn', 'Thompson',
+    'Anthony', 'Evans',
+    'Julie', 'Hall',
+    'Paula', 'Phillips',
+    'Annie', 'Hernandez',
+    'Dorothy', 'Murphy'];
