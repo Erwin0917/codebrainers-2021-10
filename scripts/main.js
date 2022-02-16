dde88bc0724ff6bc75e1d17b17e133ed9f6f3c45
kostek437 <kostek@sobanek.pl>
2022-02-16 13:47:21 +0100

    change state of cells half done

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,74 @@
-let rows = 15;
-let columns = 30;
+let rows = 15+2;
+let columns = 30+2;
 
 function createArray(rows, columns){
-    let squares = new Array(rows)
+    let array = new Array(rows)
     for(let i = 0; i < rows; i++)
     {
-        squares[i] = new Array(columns);
+        array[i] = new Array(columns);
     }
 
-    return squares
+    return array
+}
+function createLargerArray(rows, columns){
+    let array = new Array(rows)
+    for(let i = 0; i < rows; i++)
+    {
+        array[i] = new Array(columns);
+    }
+
+    return array
 }
 
-let squaresValues = createArray(rows, columns);
+let squaresValues = createLargerArray(rows, columns);
 let squares = createArray(rows, columns);
 let neighbours = createArray(rows, columns);
 
+// console.log(squaresValues)
+// console.log(neighbours)
+
 function fillArray(){
+
     for(let i = 0; i < rows; i++)
     {
         for(let j = 0; j < columns; j++)
         {
             squaresValues[i][j] = Math.floor(Math.random()*2);
-            // squaresValues[i][j] = 0
-            neighbours[i][j] = 0
+            // squaresValues[i][j] = 1;
+
 
         }
     }
+
+    for(let i = 0; i< columns;i++)
+    {
+        squaresValues[0][i] = 0;
+        squaresValues[rows-1][i] = 0;
+
+
+    }
+    for(let i = 0; i< rows;i++)
+    {
+        squaresValues[i][0] = 0;
+        squaresValues[i][columns-1] = 0;
+    }
     // console.log(squaresValues)
 }
+
+// columns-=2;
+// rows-=2;
 
 function createField(){
     fillArray();
 
     const squareContainer = document.getElementById("container");
 
-    for(let i = 0; i < rows; i++){
+    for(let i = 1; i < rows-1; i++){
         const row = document.createElement("div");
         row.classList.add("row");
         squareContainer.append(row);
 
-        for(let j = 0; j < columns; j++){
+        for(let j = 1; j < columns-1; j++){
             const newSquare = document.createElement('div');
             newSquare.classList.add('square');
             row.append(newSquare);
@@ -57,6 +86,10 @@ function createField(){
             if(squaresValues[i][j] === 1){
                 squares[i][j].classList.add("blacked");
             }
+            // if(squaresValues[i][j] === 0){
+            //     squares[i][j].classList.add("blacked");
+            // }
+
 
         }
     }
@@ -80,7 +113,7 @@ document.querySelectorAll('.square').forEach(item => {
     //if(i === 0 || i === rows || j === 0 || j === columns ){
     //
     // }
-console.log(squaresValues)
+// console.log(squaresValues)
 function neighboursCount(){
     for(let i = 1; i < rows-1; i++)
     {
@@ -100,18 +133,54 @@ function neighboursCount(){
 
 
 
-            console.log(tempNeighborsCount);
+            // console.log(tempNeighborsCount);
             neighbours[i][j] = tempNeighborsCount;
 
         }
 
 
     }
-    console.log(neighbours)
 
 }
 neighboursCount()
+console.log(squaresValues);
+const newSquaresValues = squaresValues.map(function(arr) {
+    return arr.slice();
+});
 
-function startGame(){
+function changeStateOfCell(){
+    for(let i = 1; i < rows-1; i++){
+
+        for(let j = 1; j < columns-1; j++){
+            if(neighbours[i][j] < 2){
+                squaresValues[i][j] = 0;
+            }
+
+        }
+
+    }
+
 
 }
+
+console.log(newSquaresValues);
+
+function startGame(){
+    changeStateOfCell()
+        for(let i = 0; i < rows; i++)
+        {
+            for(let j = 0; j < columns; j++)
+            {
+                if(squaresValues[i][j] === 1)
+                {
+                    squares[i][j].classList.remove("blacked");
+                }
+
+            }
+        }
+}
+
+document.querySelector('.start').addEventListener('click', () => {
+    startGame();
+    console.log("start");
+});
